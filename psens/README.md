5bcb44d430c015214d1b407bc6a2d89f665bdf27
Kcraam <kcraam@gmail.com>
2015-07-27 12:25:53 +0200

    More cleaning

--- psens/README.md
+++ psens/README.md
@@ -1,18 +1,39 @@
-RasP-TempSensor
-===============
+# Platform Sens.Solutions
 
-Temperature Sensor with a Rasberry Pi and a DHT sensor.
+Temperature Sensor Monitoring
 
-TODO
-===============
+##Installation Steps
+###Step 1:Clone the project to your application folder.
+
+    git clone https://github.com/SensSolutions/sens_platform.git
+
+###Step 2: Activate the virtual environment and install the requirements.
+ 
+     cd sens_platform
+     virtualenv env
+     source env/bin/activate
+     pip install -r requirements.txt 
+
+##TODO
+
  * Automaticaly create output name from sensor version and pin used, or by user choice.
  * Verify that multiple sensors and applications instances works correctly.
  * Correct reading errors (sensor return incorrect values).
  * Do a sensor analyse function to calibrate sensor.
  * Daemonize code (and create startup scripts).
 
-Copyright
-===============
-Parts of code are from [Adafruit's Raspberry-Pi Python Code Library](https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code) by Tony DiCola.
+##Copyright
 
-Parts from [Cacti documentation](http://docs.cacti.net/manual:088:3a_advanced_topics.1_data_input_methods#making_your_scripts_work_with_cacti)
+* Parts of code are from [Adafruit's Raspberry-Pi Python Code Library](https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code) by Tony DiCola.
+* Parts from [Cacti documentation](http://docs.cacti.net/manual:088:3a_advanced_topics.1_data_input_methods#making_your_scripts_work_with_cacti)
+* https://www.eclipse.org/paho/clients/python/docs/
+
+##TODO
+* Add protection from sensor error readings (Â X % last value)
+* Move settings to a external file
+** http://pymotw.com/2/ConfigParser/index.html
+* Capture signal handling (^C and friends)
+* Write logs somewhere?
+* Daemonize?
+** http://code.activestate.com/recipes/278731/
+** http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
--- psens/airsensor.py
+++ psens/airsensor.py
@@ -1,6 +1,5 @@
 import Adafruit_DHT
 import datetime
-import random
 import paho.mqtt.publish as publish
 import time
 import logging
@@ -9,11 +8,11 @@ from ConfigParser import SafeConfigParser
 logger = logging.getLogger('PSENSv0.1')
 
 
-def airSensor(org,place,brokerIP,clientId,cfgfile):
+def airSensor(org, place, brokerIP, clientId, cfgfile):
     logger.debug(airSensor)
 
     Config = SafeConfigParser()
-    #Config.read("tempsensor.cfg")
+    # Config.read("tempsensor.cfg")
     Config.read(cfgfile)
     
     topic      = Config.get('Broker', 'topic') 
@@ -27,7 +26,7 @@ def airSensor(org,place,brokerIP,clientId,cfgfile):
     logName    = Config.get('Log', 'logname')
 
     try:
-        #sens.solutions/pool/sensors/air/humidity
+        # sens.solutions/pool/sensors/air/humidity
         parts = topic.split('/')
         org = parts[0]
         place = parts[1]
@@ -39,8 +38,9 @@ def airSensor(org,place,brokerIP,clientId,cfgfile):
         logger.warning("Error: No topic")
 
     while True:
-        # Try to grab a sensor reading.  Use the read_retry method which will retry up
-    # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+            # Try to grab a sensor reading.  Use the read_retry
+            # method which will retry up to 15 times to get a sensor reading
+            # (waiting 2 seconds between each retry).
             humidity, temperature = Adafruit_DHT.read_retry(sensorType, pin)
 
     # Note that sometimes you won't get a reading and
@@ -52,25 +52,23 @@ def airSensor(org,place,brokerIP,clientId,cfgfile):
                 now = datetime.datetime.now()
                 hora = now.strftime("%Y-%m-%d %H:%M:%S")
 
-                log = hora + " | " + clientId + " | " + brokerIP + " : " + sensorTemp + "/" + str(temperature) +" C " + sensorHum + "/" + str(humidity) + " %\n"
+                # log = hora + " | " + clientId + " | " + brokerIP + " : " + sensorTemp + "/" + str(temperature) +" C " + sensorHum + "/" + str(humidity) + " %\n"
                 tjson = '{ "org" : "' + org + '", "place" : "' + place + '", "what" : "' + what + '", "sensor" : "' + sensorName + '", "type" : "temperature", "value" : ' + str(round(temperature,2)) + ',  "timestamp" : "' + hora + '" }'
                 hjson = '{ "org" : "' + org + '", "place" : "' + place + '", "what" : "' + what + '", "sensor" : "' + sensorName + '", "type" : "humidity", "value" : ' + str(round(humidity,2)) + ', "timestamp" : "' + hora + '" }'
 
-    #
                 logger.debug(tjson)
                 logger.debug(hjson)
-    #
 
                 publish.single(topic + sensorName + "/temperature" , tjson, hostname = brokerIP, client_id= clientId, will=None, auth=None, tls=None)
                 publish.single(topic + sensorName + "/humidity" , hjson, hostname = brokerIP, client_id= clientId, will=None, auth=None, tls=None)
 
-                if (writeLog) :
+                if (writeLog):
                     with open(logName, 'a') as logfile:
                         logfile.write(tjson + "\n" + hjson + "\n")
 
                 time.sleep(sleepTime)
             else:
-                logger.warning( "Failed to get reading. Try again!")
+                logger.warning("Failed to get reading. Try again!")
                 time.sleep(sleepTime)
 
 #
