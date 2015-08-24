b433caa43199896b6c4d6c34b941e2d7cff8a808
Kcraam Odalloc <kcraam@gmail.com>
2015-08-24 14:06:01 +0200

    Added code for dinamyc module import

--- psens/processos.py
+++ psens/processos.py
@@ -1,18 +1,37 @@
 from multiprocessing import Process
+import importlib
 import time
 import random
 
-dsensors=[{"name":"air","type":"DTH"},
-          {"name":"water","type":"DS22"},
-          {"name":"internal","type":"PSens"}]
+dsensors=[{"name":"air","type":"th","subtype":"DTH"},
+          {"name":"water","type":"time","subtype":"DS22"},
+          {"name":"internal","type":"bogus","subtype":"PSens","sleep_time":60}]
 
 def sensor(d, *o):
     try:
-        print "process name: " + d["name"]
-        print "\t Sensor type: "+d["type"]
+        ''' To avoid load in memory all sensors capture apps,
+            we load only que specific one
+            https://docs.python.org/2/library/importlib.html#importlib.import_module
+            This code isn't working yet, maybe it can be left
+            for a n+1 version
+        '''
+        my_module = importlib.import_module(d["type"],package=sensors)
+        my_module.run()
+
+    except Exception, e:
+        print "Error importing custom module: " + str(e)
+        print "No sensor control application: " + d["type"]
+        print "Using Bogus function"
+        bogus_sensor(d)
+
+def bogus_sensor(d,*o):
+    try:
         if o:
             for val in o:
                 print "Others: "+ val
+
+        print "process name: " + d["name"]
+        print "\t Sensor subtype: "+d["subtype"]
 
         t= random.randint(1,10)
         print "\tGoing to sleep for " + str(t) + "s"
