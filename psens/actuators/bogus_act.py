df8fadd644727277eb92d1862dc1fc651f0eeac4
Kcraam <kcraam@gmail.com>
2015-09-07 16:45:06 +0200

    Lots of cleaning, rearrange files and functions. Not finished yet. This Version NOT WORK

---
+++ psens/actuators/bogus_act.py
@@ -0,0 +1,39 @@
+'''
+Module containig bogus module and actuator template
+
+created on Tue Jul 29 10:12:58 2014
+
+@author: mcollado
+'''
+import time
+from random import randint
+import logging
+import sys
+
+logger = logging.getLogger('PSENSv0.1')
+
+def bogus(d, *o):
+    try:
+        if o:
+            for val in o:
+                print "Others: " + val
+
+        logger.debug("process name: %s Sensor subtype: %s driver: %s",
+                     d['name'], d['subtype'], d['driver'])
+
+# Specific code starts here
+
+        t = randint(1, 10)
+        logger.debug("%s is going to sleep for %i s", d['name'], t)
+        time.sleep(t)
+        logger.warning("%s says bye bye.", d['name'])
+
+# Specific code ends here
+
+    except Exception, err:
+        logger.warning("Error: %s", str(err))
+    except KeyboardInterrupt:
+        sys.exit(0)
+
+    return None
+
---
+++ psens/sensor.py
@@ -0,0 +1,67 @@
+"""
+This module read data from sensors loading specific driver for every sensor
+
+created on Mon Sep 07 10:12:58 2015
+
+@author: mcollado
+"""
+
+import importlib
+import logging
+# import custom modules
+from sensors import bogus
+
+logger = logging.getLogger('PSENSv0.1')
+
+
+def readSensor(SensorDict):
+    '''
+    This funtion loads correct driver for every sensor
+    and returns the read values
+    '''
+    try:
+        """
+        http://stackoverflow.com/a/5391245
+        try:
+            func = getattr(modulename, funcname)
+        except AttributeError:
+            print 'function not found "%s" (%s)' % (funcname, arg)
+        else:
+            func(arg)
+
+
+        http://stackoverflow.com/a/15004155
+        The gettattr function has an optional third argument for a default
+        value to return if the attribute does not exist, so you could use that:
+
+        fun = getattr(modulename, funcname, None)
+
+        if fun is None:
+            print 'function not found "%s" (%s)' % (funcname, arg)
+        else
+            fun(arg)
+        """
+
+        module_name = "sensors." + SensorDict['type']
+        logger.debug('Loading Sensor module %s with driver "%s"',
+                     module_name, SensorDict['driver'])
+        sen_module = importlib.import_module(module_name, package=None)
+        try:
+            # print s_module
+            # print my_module
+            sen_function = getattr(sen_module, SensorDict['driver'])
+            logger.debug("%s", sen_module)
+            # except AttributeError:
+            # logger.warning("Function not found %s (%s)", SensorDict['driver'], SensorDict)
+        except Exception, err:
+            logger.warning("Error: %s", err)
+        sen_result = sen_function(SensorDict)
+
+    except Exception, err:
+        logger.warning("Error importing custom module: %s No driver for: %s",
+                       str(err), sen_module)
+        logger.debug("Using Bogus function")
+        sen_result = bogus.bogus(SensorDict)
+
+    return sen_result
+
