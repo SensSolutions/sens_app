9ab818b647d5442765845ca369d72ead3a7c2962
Kcraam <kcraam@gmail.com>
2015-11-05 15:34:18 +0100

    start db connection 2

--- webapp/db/db.py
+++ webapp/db/db.py
@@ -1,3 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  5 10:48:58 2015
+
+@author: mcollado
+
+mysql> desc user;
+
++--------+---------+------+-----+---------+----------------+
+| Field  | Type    | Null | Key | Default | Extra          |
++--------+---------+------+-----+---------+----------------+
+| id     | int(11) | NO   | PRI | NULL    | auto_increment |
+| name   | text    | NO   |     | NULL    |                |
+| passwd | text    | NO   |     | NULL    |                |
+| email  | text    | YES  |     | NULL    |                |
++--------+---------+------+-----+---------+----------------+
+
+mysql> desc data;
++-----------+-------------+------+-----+---------+----------------+
+| Field     | Type        | Null | Key | Default | Extra          |
++-----------+-------------+------+-----+---------+----------------+
+| id        | int(11)     | NO   | PRI | NULL    | auto_increment |
+| org       | varchar(50) | YES  |     | NULL    |                |
+| place     | varchar(50) | YES  |     | NULL    |                |
+| what      | varchar(50) | YES  |     | NULL    |                |
+| sensor    | varchar(50) | YES  |     | NULL    |                |
+| type      | varchar(50) | YES  |     | NULL    |                |
+| dname     | varchar(50) | YES  |     | NULL    |                |
+| dvalue    | float       | YES  |     | NULL    |                |
+| timestamp | datetime    | YES  |     | NULL    |                |
+| payload   | text        | YES  |     | NULL    |                |
+| _dthhmm   | datetime    | YES  |     | NULL    |                |
++-----------+-------------+------+-----+---------+----------------+
+
+http://flask.pocoo.org/docs/0.10/patterns/sqlalchemy/#sql-abstraction-layer
+http://docs.sqlalchemy.org/en/rel_1_0/core/connections.html
+
+"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Text, Float, DateTime
@@ -5,6 +44,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = 'user'
@@ -14,6 +54,7 @@ class User(Base):
     email = Column(Text)
     def __repr__(self):
         return "<User(name='%s', email='%s')>" % (self.name, self.email)
+
 
 class Data(Base):
     __tablename__= 'data'
@@ -29,8 +70,8 @@ class Data(Base):
     _dthhmm = Column(DateTime)
     def __repr__(self):
         return self
-    
-        
+
+
 
 ed_user =User(name='edi', passwd='passwd', email='edsmail@mail.org')
 
